3f79c826f06e31b9c5abe3ae5e8ffd953347beb2
Chin-Hung Lee <kenny8379@gmail.com>
2023-03-01 14:27:51 -0800

    fix..

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -77,14 +77,7 @@ public class Server implements Runnable {
 		for (int a = 0; a < b; a++) {
 			result[a] = (byte) (i >> (b - 1 - a) * 8);
 		}
-		result[0] = (byte) (i >> 8);
-		result[1] = (byte) (i /* >> 0 */);
-		/*
-		 * byte[] result = new byte[4];
-		 * 
-		 * result[0] = (byte) (i >> 24); result[1] = (byte) (i >> 16); result[2] =
-		 * (byte) (i >> 8); result[3] = (byte) (i );
-		 */
+	
 		return result;
 
 	}
