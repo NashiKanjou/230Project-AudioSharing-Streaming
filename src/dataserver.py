4f1acae749eb84f177db648262c97db0879a0453
atn28 <98671416+atn28@users.noreply.github.com>
2023-03-14 16:18:54 -0700

    updated dataserver.py to use sockets

--- src/dataserver.py
+++ src/dataserver.py
@@ -1,49 +1,59 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
-import time
-import requests
+#!/usr/bin/env python3
 
-hostName = "localhost"
-serverPort = 8100
+import sys
+import socket
+import selectors
+import types
 
-class MiddleServer(BaseHTTPRequestHandler):
-
-
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def do_GET(self):
-        print(self.path)
-
-        req = str(self.path)[1:]
-
-        print("requested song is: " + req)
-       
-        #if req == 'icecream.wav':
-        #   f = open(filepath, 'r')
-        
-        filepath = "icecream.wav"
-        f = open(filepath, 'r')
-      
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(str(f), "utf-8"))
+sel = selectors.DefaultSelector()
 
 
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MiddleServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
 
-    webServer.server_close()
-    print("Server stopped.")
+
+host = '127.0.0.1'
+port = 65500
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
