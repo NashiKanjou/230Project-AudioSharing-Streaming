3301c4d0457084549bb0090a1d0304624551bd93
atn28 <98671416+atn28@users.noreply.github.com>
2023-03-15 14:54:17 -0700

    fixed stall bug client.py

--- src/client.py
+++ src/client.py
@@ -2,17 +2,46 @@
 
 import socket
 
-HOST = "127.0.0.1"  # localhost
-PORT = 65400  # middle server port
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Songname")
+#HOST = '169.234.10.40'
+#PORTS = [9991]
+
+HOST = "127.0.0.1"  # localhost
+PORTS = [65400, 65410]  # middle server ports
+
+song_name = "songname"
+
+received_data = 0
+
+for port in PORTS:
+
+    if received_data > 0:
+        break
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            print(f"Attempting connection to middle server {port}...")
+            s.connect((HOST, port))
+
+            print(f"Successfully connected to middle server {port}.")
+            s.sendall(b'songname')
     
-    while True:
-        data = s.recv(1024)
-        if not data:
-            break
-        print("got data piece")
+            test_i = 0
+            while True:
+                data = s.recv(1024)
+                if not data:
+                    print("done")
+                    break
+                test_i += 1
+                print(f"got data piece {test_i}")
+                
+            received_data = 1
+
+        except socket.error as msg:
+            print(f"Connection failed: {msg}")
+            continue
+
+
+print("all done")
 
 #print(f"Received {data!r}")
