f4285a465bc48b96c54702dc702dc5129937a8aa
Chin-Hung Lee <58142790+NashiKanjou@users.noreply.github.com>
2023-03-15 14:54:13 -0700

    Update ClientAPI.java
    newest

--- src/Client/ClientAPI.java
+++ src/Client/ClientAPI.java
@@ -1,13 +1,19 @@
 package Client;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -16,7 +22,8 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
-import audio.AudioBuffer;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import audio.AudioPlayer;
 import audio.AudioPlayerException;
 
@@ -24,10 +31,10 @@ public class ClientAPI {
 
 	public static final String host1 = "";
 	public static final int host1_port = 9991;
-	public static final String host2 = "114.33.193.52";
-	public static final int host2_port = 9991;
+	public static final String host2 = "169.234.41.69";
+	public static final int host2_port = 65400;
 
-	public static AudioBuffer buffer;
+	public static ByteArrayOutputStream buffer;
 	// private static boolean isPlaying;
 	public static AudioPlayer player;
 
@@ -36,7 +43,7 @@ public class ClientAPI {
 
 	private static final String audioFormatHeader = "AUDIOFORMAT=";
 	private static final String audioDataHeader = "AUDIODATASTART";
-	public static final int single_transfer_size = 480000;// testing
+	public static final int single_transfer_size = 2048;// testing
 
 	public static int sampleRate = 8000;
 	public static int bit = 16;
@@ -51,65 +58,88 @@ public class ClientAPI {
 
 	private static boolean done = false;
 
-	public static byte[] recieveAudioData(int frames) throws IOException {
+	public static List<String> list_files = new ArrayList<String>();
+
+	public static byte[] old_recieveAudioData(int frames) throws IOException {
 		byte[] data = new byte[frames];
 		DataInputStream input = ClientAPI.getDataInputStream();
-		int count = 0;
-		while (count < frames) {
-			data[count] = input.readByte();
-			count++;
-		}
+
+		int i = input.read(data);
+		//System.out.println(i);
+		ClientAPI.sendMessage("finish");
 		return data;
+
 	}
 
-	public static void playAudio() throws AudioPlayerException {
-		// player = new AudioPlayer(buffer);
-		// player.playAudio();
-		AudioPlayer ap = new AudioPlayer(buffer);
-		ap.playAudio();
-		/*
-		Clip clip;
-		AudioInputStream inputStream;
-		try {
-			//System.out.println(buffer.getFormat().getSampleRate());
-			inputStream = buffer.toAudioInputStream();
-			try {
-				clip = AudioSystem.getClip();
-				clip.open(inputStream);
-				clip.start();
-				/*
-				 * might need a new thread for this, please check!
-				 * 
-				 * 
-				 * 
-				 
-			} catch (LineUnavailableException e) {
-				e.printStackTrace();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+	public static void playAudio() throws AudioPlayerException, UnsupportedAudioFileException, IOException,
+			LineUnavailableException, InterruptedException {
+
+		try (ServerSocket s = new ServerSocket(9528)) {
+			Thread output = new Thread("Buffer..") {
+
+				@Override
+				public void run() {
+					try {
+						Socket buf = s.accept();
+						int last = 0;
+						// read buffer and send..
+						System.out.println("BufferThread");
+						while (true) {
+							if (map_data.containsKey(last)) {
+								byte[] data = map_data.get(last);
+								buf.getOutputStream().write(data);
+								map_data.remove(last);
+								last += data.length;
+							}
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			output.start();
 		}
-*/
+		Thread player = new Thread("player") {
+			@Override
+			public void run() {
+				try (Socket b = new Socket("", 9528)) {
+					// read from input and play
+					AudioInputStream ais = AudioSystem.getAudioInputStream(b.getInputStream());
+					try (Clip clip = AudioSystem.getClip()) {
+						//System.out.println("Player Thread");
+						clip.open(ais);
+						clip.start();
+						Thread.sleep(100); // given clip.drain a chance to start
+						clip.drain();
+						isPlaying = false;
+					} catch (LineUnavailableException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		player.start();
 	}
 
 	public static void stopAudio() throws AudioPlayerException {
 		player.stop();
 	}
 
-	/**
-	 * will create new buffer in this API with data
-	 * 
-	 * @param sampleRate is the sample rate of data
-	 * @param channels   is number of channels of the data
-	 * @param bitdepth   is bit depth of data (usually will be 16 or 24)
-	 * @param signed     is the data signed (usually signed)
-	 * @param bigEndian  is the data bigEndian or not (usually true)
-	 */
-	public static void createAudioBuffer(int sampleRate, int bitdepth, int channels, boolean signed, boolean bigEndian,
-			byte[] data) {
-		AudioFormat format = new AudioFormat(sampleRate, bitdepth, channels, signed, bigEndian);
-		buffer = new AudioBuffer(format, data);
-	}
+	public static AudioFormat format;
 
 	/**
 	 * will create new buffer in this API
@@ -122,15 +152,28 @@ public class ClientAPI {
 	 */
 	public static void createAudioBuffer(int sampleRate, int bitdepth, int channels, boolean signed,
 			boolean bigEndian) {
-		AudioFormat format = new AudioFormat(sampleRate, bitdepth, channels, signed, bigEndian);
+
+		format = new AudioFormat(sampleRate, bitdepth, channels, signed, bigEndian);
 		System.out.println("number of channel(s): " + format.getChannels());
 		System.out.println("bit depth: " + format.getSampleSizeInBits());
 		System.out.println("sample rate: " + format.getSampleRate());
-		buffer = new AudioBuffer(format);
+		// buffer = new AudioBuffer(format);
+
+		buffer = new ByteArrayOutputStream();
 	}
 
-	public static void appendAudioBuffer(byte[] data) {
-		buffer.write(data, 0, data.length);
+	public static boolean isPlaying = false;
+
+	public static HashMap<Integer, byte[]> map_data = new HashMap<Integer, byte[]>();
+	public static int count = 0;
+
+	public static void appendAudioBuffer(byte[] data, int off) throws IOException {
+		map_data.put(count, data);
+		count += data.length;
+		// buffer.write(data, 0, data.length);
+		// buffer.transcode(AudioFormat.Encoding.PCM_SIGNED);
+		// buffer.toAudioInputStream();
+		// buffer.
 	}
 
 	public static void downloadFile(String filename, int piece, int chunksize) throws IOException {// untested
@@ -227,7 +270,7 @@ public class ClientAPI {
 
 	public static void connectServer(String host, int port) {
 		try {
-			System.out.println("connecting to "+ host);
+			System.out.println("connecting to " + host);
 			Socket server = new Socket(host, port);
 			setServer(server);
 			System.out.println("connected");
@@ -270,25 +313,54 @@ public class ClientAPI {
 							ClientAPI.createAudioBuffer(sampleRate, bit, channels, signed, bigEndian);
 						} else if (line.startsWith(audioDataHeader)) {
 							try {
+								// DataInputStream input = ClientAPI.getDataInputStream();
 								System.out.println("recieving..");
-								byte[] data = ClientAPI.recieveAudioData(single_transfer_size);
-								System.out.println("appending data to buffer.." + data.length);
-								ClientAPI.appendAudioBuffer(data);
-								/*
-								 * will need to do more things about data and buffer, the code above is only for
-								 * testing and have bugs..
-								 * 
-								 * 
-								 */
-								System.out.println("playing..");
-								ClientAPI.playAudio();
-							} catch (IOException e) {
-								e.printStackTrace();
-							} catch (AudioPlayerException e) {
+								// String l = server_send.nextInt();
+								// System.out.println(l);
+								// int frames = server_send.nextInt();
+								int frames = 2048;
+								byte[] data = new byte[frames];
+								DataInputStream input = ClientAPI.getDataInputStream();
+
+								int i = input.read(data);
+								//System.out.println(i);
+								//ClientAPI.sendMessage("finish");
+								
+								//System.out.println("appending data to buffer.." + data.length);
+								ClientAPI.appendAudioBuffer(data, 0);
+								playAudio();
+
+								while (true) {
+									// l = server_send.nextLine();
+									// System.out.println(l);
+									// frames = server_send.nextInt();
+									data = new byte[frames];
+									i = input.read(data);
+									//System.out.println(i);
+									//ClientAPI.sendMessage("finish");
+									ClientAPI.appendAudioBuffer(data, 0);
+									//System.out.println("appending data to buffer.." + data.length);
+									if (i != 2048) {
+										break;
+									}
+								}
+								// InputStream in = new BufferedInputStream(ClientAPI.getDataInputStream());
+								// play(in);
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
+						} else if (line.equalsIgnoreCase("filelistheader")) {
+							list_files.clear();
+							line = server_send.nextLine();
+							while (!line.equalsIgnoreCase("filelistend")) {
+								list_files.add(line);
+								// System.out.println(line);
+								line = server_send.nextLine();
+							}
+							// output to GUI?
 						} else {
-
+							// System.out.println(line);
 						}
 					}
 				}
