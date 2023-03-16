18dbcabe07e464a72ee0c92ae06ee8679768d25c
Chin-Hung Lee <58142790+NashiKanjou@users.noreply.github.com>
2023-03-16 00:48:01 -0700

    Update data-server.py

--- src/Server/data-server.py
+++ src/Server/data-server.py
@@ -8,7 +8,7 @@ HOST = "127.0.0.1"  # localhost
 MY_PORT = 65500 
 
 songlist = []
-for n in os.listdir("src/Server/database"):
+for n in os.listdir("./database"):
     if n.endswith('.wav'):
         songlist.append(n)
 
@@ -17,7 +17,7 @@ songlist_str = "liststart\r\n"
 for s in songlist:
     songlist_str = songlist_str + s + "\r\n"
 
-songlist_str = songlist_str + "listend"
+songlist_str = songlist_str + "listend"+ "\r\n"
 
 print(songlist)
 
@@ -27,29 +27,33 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         s.listen()
         print(f"Data Server - listening on port {MY_PORT}")
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            
-            request = conn.recv(2048)
-            req = str(request)[2:len(str(request))-5] # trim b' ' from string
-
-            if req in songlist:
-                print("Looking for song: " + req)
-                with open(os.path.join("src/Server/database", req), 'rb') as f:
-                    while True:
-                        data = f.read(2048)
-                        if len(data) < 1:
-                            print("Done sending WAV file.")
-                            break
-                        conn.send(data)
-
-            elif 'list' in req:
-                print("Sending song list.")
-                conn.send(bytes(songlist_str, 'utf-8'))
-
-            else:
-                print(f"Requested file {req} not found.")
-                conn.send(b"Requested file not found.")
-                
-
+        while True:
+            conn, addr = s.accept()
+            try:
+                with conn:
+                    print(f"Connected by {addr}")
+                    
+                    request = conn.recv(2048)
+                    req = str(request)[2:len(str(request))-5] # trim b' ' from string
+        
+                    if req in songlist:
+                        print("Looking for song: " + req)
+                        with open(os.path.join("./database", req), 'rb') as f:
+                            #conn.sendall(b'audio\r\n')
+                            
+                            while True:
+                                data = f.read(2048)
+                                if len(data) < 1:
+                                    print("Done sending WAV file.")
+                                    break
+                                conn.send(data)
+        
+                    elif 'list' in req:
+                        print("Sending song list.")
+                        conn.send(bytes(songlist_str, 'utf-8'))
+        
+                    else:
+                        print(f"Requested file {req} not found.")
+                        conn.send(b"Requested file not found.")
+            except Exception:
+                print("error")
