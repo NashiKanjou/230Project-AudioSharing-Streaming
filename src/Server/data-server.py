cbef7104ad7e509e438dcb64d9df63e1b867d9be
Quang Anh Hoang <hoangqa@uci.edu>
2023-03-19 19:12:53 -0700

    Add cache and request handlers to middle server

--- src/Server/data-server.py
+++ src/Server/data-server.py
@@ -1,98 +1,72 @@
-#!/usr/bin/env python3
-
-import socket
-import wave
 import os
-import math
-HOST = "127.0.0.1"  # localhost
-MY_PORT = 65500 
+import socket
+import select
+import sys
 
-songlist = []
-for n in os.listdir("./database"):
-    if n.endswith('.wav'):
-        songlist.append(n)
+HOST = "127.0.0.1"
+PORT = 65500
+SIZE = 2048
+FORMAT = "utf-8"
 
-songlist_str = "liststart\r\n"
+class DataServer:
+    def __init__(self):
+        self.addr = (HOST, PORT)
+        self.playlist = []
+        self.path = "database"
 
-for s in songlist:
-    songlist_str = songlist_str + s + "\r\n"
+    def parse_directory(self):
+        playlist_str = "playlist - "
+        for file in os.listdir(os.path.join(os.getcwd(), self.path)):
+            if file.endswith('.wav'):
+                self.playlist.append(file)
+                playlist_str = playlist_str + file + "; "
+        playlist_str = playlist_str + "\r\n"
+        return playlist_str      
 
-songlist_str = songlist_str + "listend"+ "\r\n"
+    def send_data(self, filename):
+        with open(os.path.join(os.getcwd(), self.path, filename), 'rb') as file:
+            chunk_size = SIZE - len('caching ' + filename)
+            data = file.read(chunk_size)
+            while len(data) > 0:
+                data = bytes('caching ' + filename, FORMAT) + data
+                self.server_connection.send(data)
+                data = file.read(chunk_size)
+            print(f"Done sending {filename} to middle server")
+            self.server_connection.send(bytes('cacheend ' + filename, FORMAT))
 
-print(songlist)
+    def parse_message(self, data):
+        messages = data.splitlines()
+        for msg in messages:
+            if (msg == "playlist"):
+                playlist_str = self.parse_directory()
+                self.server_connection.send(playlist_str.encode(FORMAT))
+                print("playlist sent\n")
+            elif (msg in self.playlist):
+                self.send_data(msg)
+            else:
+                print(msg)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, MY_PORT))
-    
-    while True:
-        s.listen()
-        print(f"Data Server - listening on port {MY_PORT}")
+
+    def data_server_program(self):
+        self.server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_connection.connect(self.addr)
+
         while True:
-            conn, addr = s.accept()
-            try:
-                with conn:
-                    print(f"Connected by {addr}")
-                    
-                    request = conn.recv(2048)
-                    req = str(request)[2:len(str(request))-5] # trim b' ' from string
-                    stamp = -1
-                    print(req)
-                    if req.startswith("download"):
-                        req = req[9:]
-                        if req in songlist:
-                            conn.send(bytes('download '+req+'\r\n', 'utf-8'))
-                            print("Looking for song: " + req)
-                            size = os.path.getsize("./database/"+ req)
-                            int_piece = int(math.ceil((size/2048.0)))
-                            print("piece="+str(int_piece))
-                            conn.send(int_piece.to_bytes(4, 'big'))
-                    else:
-                        if req.endswith(".wav")==False:
-                            arr = req.split(".wav")
-                            req = arr[0]+".wav"
-                            stamp = int(arr[1])
-                        if req in songlist:
-                            if stamp == -1:
-                                conn.send(bytes('stream\r\n', 'utf-8'))
-                            print("Looking for song: " + req+str(stamp))
-                            with open(os.path.join("./database/"+ req), 'rb') as f:
-                                print("file open")
-                                #conn.sendall(b'audio\r\n')
-                                int_count=0
-                                while True:
-                                    data = f.read(2048)
-                                    if len(data) < 1:
-                                        print("Done sending WAV file.")
-                                        break
-                                    #print('read'+str(len(data)))
-                                    if(stamp==-1):
-                                        conn.send(data)
-                                    else:
-                                        #data.extend(stamp.to_bytes(4, 'big'))
-                                        #chunk = bytes[len(data)+4]
-                                        #c=0
-                                        #for b in data:
-                                        #    chunk[c]=b
-                                        #    c+=1
-                                        #print(data+stamp.to_bytes(4, 'big'))
-                                        #for b in stamp.to_bytes(4, 'big'):
-                                        #    chunk[c]=b
-                                        #    c+=1
-                                        #conn.send(data)
-                                        #conn.send(stamp.to_bytes(4, 'big'))
-                                        f.seek(stamp*2048,0)
-                                        data = f.read(2048)
-                                        conn.send(data+stamp.to_bytes(4, 'big'))
-                                        print("Done sending WAV file. tag:"+str(stamp))
-                                        break;
-                                    
-                                    int_count+=1
-                        elif 'list' in req:
-                            print("Sending song list.")
-                            conn.send(bytes(songlist_str, 'utf-8'))
-            
-                        else:
-                            print(f"Requested file {req} not found.")
-                            conn.send(b"Requested file not found.")
-            except Exception as e:
-                print(e)
+            sockets_list = [self.server_connection]
+            read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+
+            for connection in read_sockets:
+                if connection == self.server_connection:
+                    data = connection.recv(SIZE).decode(FORMAT)
+                    self.parse_message(data)
+                else:
+                    message = sys.stdin.readline()
+                    self.server_connection.send(message.encode(FORMAT))
+                        
+
+        server_connection.close()
+
+
+if __name__ == "__main__":
+    dataServer = DataServer()
+    dataServer.data_server_program()
--- src/Server/middle-server.py
+++ src/Server/middle-server.py
@@ -1,55 +1,196 @@
-#!/usr/bin/env python3
-
 import socket
+import sys
+import math
+from threading import Lock
+from threading import Thread
 
-HOST = "127.0.0.1"  # localhost
-MY_PORT = 65400
-DS_PORTS = [65500]
+HOST = "127.0.0.1"
+DATA_PORT = 65400
+CLIENT_PORT = 65500
+SIZE = 2048
+FORMAT = "utf-8"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, MY_PORT))
+class MiddleServer:
+    def __init__(self):
+        self.data_servers = {}
+        self.clients = {}
+
+        self.request_queue = {}
+
+        self.playlist = {}
+        self.playlist_lock = Lock()
+        self.playlist_updated = False
+
+        self.waitCache = 0
+        self.cache = {}
+
+
+    def update_playlist(self, tid, message, lock):
+        self.playlist_updated = False
+        message = message.strip();
+        list = message.split('-')[1].split(';')
+        for file in list:
+            file = file.strip()
+            if file != '':
+                if file in self.playlist:
+                    value = self.playlist.get(file)
+                    if tid not in value:
+                        value.append(tid)
+                else:
+                    self.playlist[file] = [tid]
+
+        self.playlist_updated = True
+
+
+    def send_data(self, conn, filename):
+        data = self.cache[filename]
+        for i in range(0, int(math.ceil(len(data) / SIZE))):
+            chunk = data[i*SIZE : (i+1)*SIZE - 1]
+            conn.send(chunk)
+        print(f"Finished sending {filename}")
+
+
+    def send_data_chunk(self, conn, filename, index):
+        data = self.cache[filename]
+        chunk = data[index*SIZE : (index + 1)*SIZE - 1]
+        conn.send(chunk + index.to_bytes(4, 'big'))
+
+
+    def handle_request(self, tid, conn, request):
+
+        ## middle <-> data server
+        if (request[0:8] == b'playlist'):
+            request = request.decode(FORMAT)
+            self.update_playlist(tid, request, self.playlist_lock)
+        elif (request[0:8] == b'caching '):
+            metadata = request.split(b'.wav')[0]
+            data = request.split(b'.wav')[1]
+            filename = str(metadata[8:])[2:len(str(metadata[8:]))-1] + ".wav"
+            if filename not in self.cache.keys():
+                self.cache[filename] = data
+            else:
+                self.cache[filename] += data
+        elif (request[0:8] == b'cacheend'):
+            self.waitCache = 0
+            print("Done caching")
+
+        ## middle <-> client
+        elif (request[0:4] == b'list'):
+            playlist_str = "liststart\r\n"
+            for key in self.playlist.keys():
+                playlist_str += key + "\r\n"
+            playlist_str += "listend\r\n"
+            conn.send(bytes(playlist_str, 'utf-8'))            
+        elif (request[0:8] == b'download'):
+            request = request.decode(FORMAT)
+            conn.send(request.encode(FORMAT))
+
+            filename = request.split(' ')[1].strip()
+            if filename in self.cache.keys():
+                num_pieces = int(math.ceil(len(self.cache[filename]) / float(SIZE)))
+                conn.send(num_pieces.to_bytes(4, 'big'))
+            else:
+                try:
+                    data_client_tid = self.playlist[filename][0]
+                    if (data_client_tid == tid):
+                        conn.send(filename.encode(FORMAT))
+                    else:
+                        data_command_queue = self.request_queue[data_client_tid]
+                        data_command_queue.append(filename)
+
+                except Exception:
+                    print(f"File {filename} is not available")
+                # find the thread and port communicate with data server
+                # holding this file and push a command into its queue
+                # for execution
+        else:
+            print(request)
+            request = str(request)
+            if (request.startswith("b'")):
+                request = request[2:len(str(request)) - 1]
+            if (request.endswith("\\r\\n")):
+                request = request[0:len(str(request)) - 4]
+
+            stamp = -1
+            if request.endswith('.wav') == False:
+                arr = request.split('.wav')
+                request = arr[0] + '.wav'
+                stamp = int(arr[1])
+            if request in self.playlist.keys():
+                if request in self.cache.keys():
+                    if stamp == -1:
+                        conn.send(bytes('stream\r\n', FORMAT))
+                        self.send_data(conn, request)
+                    else:
+                        self.send_data_chunk(conn, request, stamp)
+                else:
+                    data_client_tid = self.playlist[request][0]
+                    if (data_client_tid == tid):
+                        conn.send(request.encode(FORMAT))
+                    else:
+                        data_command_queue = self.request_queue[data_client_tid]
+                        data_command_queue.append(request)
+                        
+                        self.waitCache = tid
+                        client_command_queue = self.request_queue[tid]
+                        client_command_queue.append(request)
+
+            else:
+                print("Invalid request, file not available")
+
     
-    while True:
+    def executer_thread(self, conn, addr):
+        while True:
+            if len(self.request_queue[addr[1]]) > 0:
+                if (self.waitCache == addr[1]):
+                    continue
+                request = self.request_queue[addr[1]].pop(0)
+                self.handle_request(addr[1], conn, request)
+            # else:
+            #     print(self.request_queue[addr])
+            
+                    
+
+
+    def receiver_thread(self, conn, addr):
+        self.request_queue[addr[1]] = []
+
+        conn.send(("Client has successfully connected to server\r\n").encode(FORMAT))
+        conn.send(("playlist\r\n").encode(FORMAT))
+
+        Thread(target=self.executer_thread, args=(conn, addr)).start()
+
+        while True:
+            try:
+                message = conn.recv(SIZE)
+                if message:
+                    self.request_queue[addr[1]].append(message)
+
+            except:
+                continue
+
+
+    def thread_program(self, host, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, port))
         s.listen()
-        print(f"Middle Server - listening on port {MY_PORT}")
-        try:
+        
+        while True:
             conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
+            if port == DATA_PORT:
+                self.data_servers[addr[1]] = conn
+            elif port == CLIENT_PORT:
+                self.clients[addr[1]] = conn
 
-                while True:
-                    client_request = conn.recv(2048)
+            Thread(target=self.receiver_thread, args=(conn, addr)).start()
 
-                    received_data = 0
+    def middle_server_program(self):
+        Thread(target=self.thread_program, args=(HOST, DATA_PORT)).start()
+        Thread(target=self.thread_program, args=(HOST, CLIENT_PORT)).start()
 
-                    for port in DS_PORTS:
 
-                        if received_data > 0:
-                            break
 
-                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as t:
-                            try:
-                                print(f"Attempting connection to data server {port}...")
-                                t.connect((HOST, port))
-
-                                print(f"Successfully connected to data server {port}...")
-                                t.sendall(client_request)
-
-                                while True:
-                                    data = t.recv(2052)
-                                    if not data:
-                                        break
-                                    conn.sendall(data)
-                                    print('send')
-                                received_data = 1
-                                print(f"Finished sending data to client {addr}.")
-
-                            except socket.error as msg:
-                                print(f"Connection failed: {msg}")
-                                continue
-        except Exception:
-            print("error")
-
-                
-
+if __name__ == "__main__":
+    middleServer = MiddleServer()
+    middleServer.middle_server_program()
 
